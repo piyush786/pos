ad94e0633bec25c3365c54828b366d7d64361556
piyush kapoor <piyushkapoor786@Gmail.com>
2018-12-06 19:02:06 +0530

    first commit

---
+++ precache-manifest.9473d2b9934f17c072066651faa977c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f32621a4c5fcc2e78a9673db952e65a",
+    "url": "/static/media/Aryan-Holding-Group-Logo.0f32621a.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4fbe50c97fadcc6a3a0b",
+    "url": "/static/js/main.4fbe50c9.chunk.js"
+  },
+  {
+    "revision": "bd7ffb6c5539e882b0e7",
+    "url": "/static/js/1.bd7ffb6c.chunk.js"
+  },
+  {
+    "revision": "4fbe50c97fadcc6a3a0b",
+    "url": "/static/css/main.0a8cd109.chunk.css"
+  },
+  {
+    "revision": "bd7ffb6c5539e882b0e7",
+    "url": "/static/css/1.4ae1b181.chunk.css"
+  },
+  {
+    "revision": "53476d5af3518d3ea4eea436f5112d10",
+    "url": "/index.html"
+  }
+];
